c193d1a766bf09e45ee7d2cb921b64ef5600c7b3
namandhingra19 <namandhingra90@gmail.com>
2024-07-02 22:26:26 +0530

    chore: Add dotenv package for environment variable management

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import { connect } from "mongoose";
 import roomRouter from "./routes/room.route";
@@ -11,9 +14,7 @@ const PORT = 5001;
 
 app.use(express.json());
 app.use("/api/rooms", roomRouter);
-connect(
-  "mongodb+srv://naman12:spW7F8n2bIrWoZSD@cluster0.9rzomve.mongodb.net/debuide?retryWrites=true&w=majority&appName=Cluster0"
-)
+connect(process.env.MONGO_URI)
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
   )
